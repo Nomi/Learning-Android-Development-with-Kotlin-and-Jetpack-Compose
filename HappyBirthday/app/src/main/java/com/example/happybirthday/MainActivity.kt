0ad6d436ed8dbd9ea2c30eb19b781a4511e5504c
Noman <baba.noman@live.com>
2024-03-30 05:00:40 +0100

    Updated HappyBirthday
    * Implemented and moved to GreetImage() (as per Google's compose introductory course but with some modifications thought up by me).
    * For GreetText(), Fixed reliance on boxes and used Columns/Rows along with their vertical/horizontal Arrangement and horizontal/vertical Alignment.
    * Other fixes, improvements, and changes to GreetText().

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,22 +3,30 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -38,20 +46,55 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(message = "Happy Birthday Bob!", from = "Wendy", modifier = Modifier.padding(8.dp))
+                    Box(modifier = Modifier.fillMaxSize().background(color = Color.Transparent))
+                    {
+                        HappyBirthdayTheme {
+                            GreetingAppImage(
+                                message = "Happy Birthday Bob!",
+                                from = "Wendy",
+                                modifier = Modifier.fillMaxSize().padding(0.dp)
+                                    .align(Alignment.Center)
+                            )
+                        }
+                    }
                 }
             }
         }
     }
 }
 @Composable
-fun GreetingText(message: String, from:String, modifier: Modifier = Modifier) {
-    Column (
-        modifier = modifier.fillMaxSize(),
-//        verticalArrangement = Arrangement.Center,
+fun GreetingTextPlain(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
     ) {
-        Box(
-            modifier = modifier.weight(0.8F).fillMaxWidth()
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "From $from",
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
+}
+
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .weight(0.8F)
+                .fillMaxWidth()
         )
         {
             Text(
@@ -69,19 +112,29 @@ fun GreetingText(message: String, from:String, modifier: Modifier = Modifier) {
                         tileMode = TileMode.Mirror
                     ),
                 ),
-                modifier = Modifier.align(alignment = Alignment.Center)
-
+                modifier = Modifier
+                    //.align(alignment = Alignment.Center)
+                    .background(
+                        MaterialTheme.colorScheme.primaryContainer.copy(alpha = 0.2F),
+                        RoundedCornerShape(25)
+                    )
             )
         }
-        Box(modifier=modifier.weight(0.2F).fillMaxWidth())
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.Bottom,
+            modifier = Modifier
+                .weight(0.2F)
+                .fillMaxWidth()
+        )
         {
             Text(
-                text="❤",
+                text = "❤",
                 fontSize = 18.sp,
                 textAlign = TextAlign.Center,
-                modifier = modifier.align(alignment = Alignment.BottomStart)
-                    //.padding(8.dp)
-                )
+                //modifier = Modifier.align(alignment = Alignment.BottomStart)
+                //.padding(8.dp)
+            )
             Text(
                 text = "From $from",
                 fontSize = 36.sp,
@@ -90,21 +143,68 @@ fun GreetingText(message: String, from:String, modifier: Modifier = Modifier) {
                 textAlign = TextAlign.Right,
                 color = MaterialTheme.colorScheme.secondary,//Color.DarkGray,
 //                modifier = Modifier.align(alignment = Alignment.End)
-                modifier = modifier.align(alignment = Alignment.BottomEnd)
-                    //.padding(8.dp)
+                modifier = Modifier
+                    //.align(alignment = Alignment.BottomEnd)
+                    .background(
+                        color = MaterialTheme.colorScheme.secondaryContainer.copy(alpha = 0.2F),
+                        shape = RoundedCornerShape(25)
+                    )
+                //.padding(8.dp)
             )
         }
 
     }
 }
 
-@Preview(showBackground = true, name="Birthday Card Preview")
+@Composable
+fun GreetingAppImage(
+    message: String,
+    from: String,
+    modifier: Modifier
+) { //apparently @Composable functions with a return value should start with a lowercase letter? (like painterResource) What's the use? (apparently they don't draw?)
+    val imgPainter: Painter = painterResource(id = R.drawable.androidparty)
+    Box(
+        modifier = modifier
+    ) {
+        Image(
+            painter = imgPainter,
+            contentDescription = null, //the image in this app is only included for decorative purposes. The addition of a content description for the image would make it harder to use with TalkBack in this particular case. Instead of setting the content description that's announced to the user, you can set the image's contentDescription argument to null so that TalkBack skips the Image composable.
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F,
+            alignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+        )
+        GreetingText(message = message, from = from, modifier = Modifier.fillMaxSize().align(Alignment.Center))
+    }
+
+}
+
+@Preview(showBackground = true, name = "Birthday Card Preview")
 @Composable
 fun BirthdayCardPreview() {
-    HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Bob!", from = "Wendy", modifier = Modifier.padding(8.dp))
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Box(modifier = Modifier.fillMaxSize().background(color = Color.Transparent))
+        {
+            HappyBirthdayTheme {
+//                        GreetingText(
+//                            message = "Happy Birthday Bob!",
+//                            from = "Wendy",
+//                            modifier = Modifier.padding(8.dp)//.fillMaxSize(0.94F)
+//                        )
+                GreetingAppImage(
+                    message = "Happy Birthday Bob!",
+                    from = "Wendy",
+                    modifier = Modifier.fillMaxSize().padding(0.dp).align(Alignment.Center)
+                )
+            }
+        }
     }
 }
+
+
 
 
 /* // Their solution:
